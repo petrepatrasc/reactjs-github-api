19c8cb519b45c07c382924a07ba16d741f997bac
Petre Pătrașc <petre@dreamlabs.ro>
2015-05-03 00:47:07 +0300

    Add tests to check that dispatching events works correctly

---
+++ __tests__/stores/profile-store-test.js
@@ -0,0 +1,35 @@
+describe('Profile Store', function() {
+    jest.dontMock('../../src/js/stores/profile-store');
+    jest.dontMock('object-assign');
+
+    var AppDispatcher;
+    var callback;
+    var ProfileStore;
+
+    var AppConstants = require('../../src/js/constants/app-constants');
+
+    // mock actions inside dispatch payloads
+    var actionTodoCreate = {
+        source: 'VIEW_ACTION',
+        action: {
+            actionType: AppConstants.RETRIEVE_PROFILE,
+            username: 'petrepatrasc'
+        }
+    };
+
+    beforeEach(function() {
+        AppDispatcher = require('../../src/js/dispatcher/app-dispatcher');
+        ProfileStore = require('../../src/js/stores/profile-store');
+        callback = AppDispatcher.register.mock.calls[0][0];
+    });
+
+    it('registers a callback with the dispatcher', function() {
+        expect(AppDispatcher.register.mock.calls.length).toBe(1);
+    });
+
+    it('stores the required username when a new profile needs to be retrieved', function() {
+        callback(actionTodoCreate);
+
+        expect(ProfileStore.getUsername()).toEqual('petrepatrasc');
+    });
+});
