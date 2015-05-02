08d8b970bcf1c7e4620c202494ec17ee70b4299f
Petre Pătrașc <petre@dreamlabs.ro>
2015-05-03 01:57:27 +0300

    Add format fix

--- __tests__/stores/profile-store-test.js
+++ __tests__/stores/profile-store-test.js
@@ -1,4 +1,4 @@
-describe('Profile Store', function() {
+describe('Profile Store', function () {
     jest.dontMock('../../src/js/stores/profile-store');
     jest.dontMock('object-assign');
 
@@ -17,17 +17,17 @@ describe('Profile Store', function() {
         }
     };
 
-    beforeEach(function() {
+    beforeEach(function () {
         AppDispatcher = require('../../src/js/dispatcher/app-dispatcher');
         ProfileStore = require('../../src/js/stores/profile-store');
         callback = AppDispatcher.register.mock.calls[0][0];
     });
 
-    it('registers a callback with the dispatcher', function() {
+    it('registers a callback with the dispatcher', function () {
         expect(AppDispatcher.register.mock.calls.length).toBe(1);
     });
 
-    it('stores the required username when a new profile needs to be retrieved', function() {
+    it('stores the required username when a new profile needs to be retrieved', function () {
         callback(actionTodoCreate);
 
         expect(ProfileStore.getUsername()).toEqual('petrepatrasc');
